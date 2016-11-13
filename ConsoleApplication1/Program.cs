fa8ad34962c4c872b85ac253e5ae1284cef360fa
Vladumurovu4 <Vladumurovu4@gmail.com>
2016-11-13 14:05:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TriangleTask
+{
+    class Program
+    {
+        static private int[,] readInput(string filename)
+        {
+            string line;
+            string[] linePieces;
+            int lines = 0;
+
+            StreamReader r = new StreamReader(filename);
+            while ((line = r.ReadLine()) != null)
+            {
+                lines++;
+            }
+
+            int[,] inputTriangle = new int[lines, lines];
+            r.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            int j = 0;
+            while ((line = r.ReadLine()) != null)
+            {
+                linePieces = line.Split(' ');
+                for (int i = 0; i < linePieces.Length; i++)
+                {
+                    inputTriangle[j, i] = int.Parse(linePieces[i]);
+                }
+                j++;
+            }
+            r.Close();
+            return inputTriangle;
+        }
+        static void Main(string[] args)
+        {
+            string filename = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\input.txt";
+            int[,] inputTriangle = readInput(filename);
+            int lines = inputTriangle.GetLength(0);
+
+            for (int i = lines - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    inputTriangle[i, j] += Math.Max(inputTriangle[i + 1, j], inputTriangle[i + 1, j + 1]);
+                }
+            }
+
+            
+           Console.WriteLine(inputTriangle[0,0]);
+            
+            Console.ReadKey();
+        }
+    }
+}
